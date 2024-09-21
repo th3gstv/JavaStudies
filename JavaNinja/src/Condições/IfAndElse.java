a268b81b9b31a92459680d023ce2ce431756018a
Gustavo Augusto <gstvoaugusto@gmail.com>
2024-09-21 17:26:45 -0300

    learning if and else

--- JavaNinja/src/Condições/IfAndElse.java
+++ JavaNinja/src/Condições/IfAndElse.java
@@ -1,4 +1,27 @@
 package Condições;
 
 public class IfAndElse {
+    public static void main(String[] args) {
+
+        /*
+        IF e ELSE - Condições
+        Objetivo: Passar o ninja de nível de acordo com o número de missões
+         */
+
+        String nome = "Sasuke Uchiha";
+        String rank;
+
+        int idade = 12;
+        boolean Hokage = false;
+        short numeroDeMissoes = 24;
+
+
+        if (numeroDeMissoes >= 10 && idade > 15) {
+            System.out.println("Graduação: Chunnin");
+        } else if (numeroDeMissoes >=20) {
+            System.out.println("Graduação: Jounnin");
+        }else {
+            System.out.println("Graduação: Gennin");
+        }
+    }
 }
