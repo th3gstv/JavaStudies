895de49d50dc02e1976d6a78cbc310f06adb6ea9
Gustavo Augusto <gstvoaugusto@gmail.com>
2024-09-22 09:02:45 -0300

    Create Desafio01.java

---
+++ JavaNinja/src/Condições/Desafio01.java
@@ -0,0 +1,53 @@
+package Condições;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        //NINJA 1
+        String ninja1 = "Sasuke Uchiha";
+            int idadeN1 = 17;
+            String aldeiaN1 = "Konohagakure";
+            String missaoN1 = "Auxiliar Iruka na formação de novos gennins";
+            char rankMissaoN1 = 'D';
+            String statusMissaoN1 = "Em andamento";
+
+        System.out.println("Nome do ninja: " + ninja1);
+        System.out.println("Idade do ninja: " + idadeN1);
+        System.out.println("Aldeia do ninja: " + aldeiaN1);
+        System.out.println("Missão: " + missaoN1);
+        System.out.println("Rank da Missão: " + rankMissaoN1);
+        System.out.println("Status da missão: " + statusMissaoN1);
+        System.out.println("--------------");
+
+        //NINJA 2
+        String ninja2 = "Shikamaru Nara";
+            int idadeN2 = 18;
+            String aldeiaN2 = "Konohagakure";
+            String missaoN2 = "Limpar os arredores da vila";
+            char rankMissaoN2 = 'D';
+            String statusMissaoN2 = "Não concluída";
+        //System.out.println(ninja2 + " possui " + idadeN2 + " anos. Sua missão é: " + missaoN2 + " - " + rankMissaoN2);
+        System.out.println("Nome do ninja: " + ninja2);
+        System.out.println("Idade do ninja: " + idadeN2);
+        System.out.println("Aldeia do ninja: " + aldeiaN2);
+        System.out.println("Missão: " + missaoN2);
+        System.out.println("Rank da Missão: " + rankMissaoN2);
+        System.out.println("Status da missão: " + statusMissaoN2);
+        System.out.println("--------------");
+
+        //NINJA 3
+        String ninja3 = "Kakashi Hatake";
+            int idadeN3 = 29;
+            String aldeiaN3 = "Konohagakure";
+            String missaoN3 = "Interceptar Invasores da Aldeia da Névoa";
+            char rankMissaoN3 = 'B';
+            String statusMissaoN3 = "Concluída";
+        System.out.println("Nome do ninja: " + ninja3);
+        System.out.println("Idade do ninja: " + idadeN3);
+        System.out.println("Aldeia do ninja: " + aldeiaN3);
+        System.out.println("Missão: " + missaoN3);
+        System.out.println("Rank da Missão: " + rankMissaoN3);
+        System.out.println("Status da missão: " + statusMissaoN3);
+        System.out.println("--------------");
+
+    }
+}
